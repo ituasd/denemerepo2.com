73e2831a97e797688965e8cfee7afadeb3039d58
ituasd <turpturpturpcuk@gmail.com>
2026-01-22 15:01:05 +0300

    numpy deneme2 iki ayri arrayi sutun halinde tek arraya cevirme

---
+++ script1.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+a = np.array([1,2,5,6,7])
+a = a*8
+b  = np.array([4,5,8,6,8])
+b = b-3
+c = np.column_stack((a,b))#iki ayri arrayi birer sütun olarak tek bir array haline getirmeye vesile olyor
+cc = pd.DataFrame(c)#parametre olarak array alabiliyor it can take an array as a parameter
+cc.columns = ["X","Y"]
+print(cc)
